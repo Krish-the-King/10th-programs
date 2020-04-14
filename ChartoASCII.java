a798e3fc6954360977e7c29810a018b6b64ed6ca
Krishnansh Sood <krishnansh.sood@gmail.com>
2020-04-14 01:42:12 -0700

    Ma'am I tried converting ch to int without typecasting and it worked

---
+++ ChartoASCII.java
@@ -0,0 +1,13 @@
+public class ChartoASCII
+{
+    public static void main(String args[])
+    {
+        char chA='A',chZ='Z',cha='a',chz='z';
+        int ascA,ascZ,asca,ascz;
+        ascA=chA;
+        ascZ=chZ;
+        asca=cha;
+        ascz=chz;
+        System.out.println(ascA + "\n" + ascZ + "\n" + asca + "\n" + ascz);
+    }
+}
